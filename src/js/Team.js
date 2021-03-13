855b3213d09aa323e2a1dbfbf85f50a88f3a1ba2
avsudnichnikov <avsudnichnikov@gmail.com>
2021-03-13 15:09:12 +0500

    task #5 complete

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,3 +1,19 @@
 export default class Team {
+  constructor() {
+    this.persons = [];
+  }
 
+  get length() {
+    return this.persons.length;
+  }
+
+  add(member) {
+    this.persons.push(member);
+  }
+
+  * [Symbol.iterator]() {
+    for (let key = 0; key < this.length; key += 1) {
+      yield new Person(this.persons[key]);
+    }
+  }
 }
--- src/js/generators.js
+++ src/js/generators.js
@@ -1,9 +1,23 @@
 import {randInt} from "./utils";
+import Team from "./Team";
+import PositionedCharacter from "./PositionedCharacter";
 
 export function* characterGenerator(allowedTypes, maxLevel) {
   yield new allowedTypes[randInt(allowedTypes.length)](randInt(maxLevel) + 1);
 }
 
-export function generateTeam(allowedTypes, maxLevel, characterCount) {
-  // TODO: write logic here
+function generatePosition(boardSize, leftSide) {
+  const col = ((leftSide) ? 0 : boardSize - 2) + randInt(1);
+  const row = randInt(boardSize - 1);
+  return col + row * boardSize;
+}
+
+export function generateTeam(allowedTypes, maxLevel, characterCount, boardSize) {
+  const team = new Team();
+  for (let item = 1; item <= characterCount; item += 1) {
+    const character = characterGenerator(allowedTypes, maxLevel).next().value;
+    const position = generatePosition(boardSize, true).next().value;
+    team.add(new PositionedCharacter(character, position));
+  }
+  return team;
 }
